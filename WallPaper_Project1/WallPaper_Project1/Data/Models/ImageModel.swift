bdf3cbf376daee03ea92b8d30a6ebd384a5a1018
ThaiSun* <dao.duy.thai@sun-asterisk.com>
2023-01-06 11:04:45 +0700

    Apply MVC Structure

---
+++ WallPaper_Project1/WallPaper_Project1/Data/Models/ImageModel.swift
@@ -0,0 +1,8 @@
+//
+//  ImageModel.swift
+//  WallPaper_Project1
+//
+//  Created by DuyThai on 06/01/2023.
+//
+
+import Foundation
--- WallPaper_Project1/WallPaper_Project1/Scenes/ViewController.swift
+++ WallPaper_Project1/WallPaper_Project1/Scenes/ViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Duy Thái on 06/01/2023.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
