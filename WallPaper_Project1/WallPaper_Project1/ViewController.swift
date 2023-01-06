e38b62bcfd52b47fe5af6a2c744792f3b2701f30
ThaiSun* <dao.duy.thai@sun-asterisk.com>
2023-01-06 09:45:33 +0700

    init project

---
+++ WallPaper_Project1/WallPaper_Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallPaper_Project1
+//
+//  Created by Duy Thái on 06/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
